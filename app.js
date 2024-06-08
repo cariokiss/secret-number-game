8174c0d80df564379017bad95efbdc7aac8850e0
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-08 13:35:37 -0300

    feat: criando um número secreto aleatório através de uma função com retorno

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     const campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto')
-exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100')
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10')
 
 function verificarChute() {
-    console.log('O botão foi clicado')
+    console.log(numeroSecreto)
+}
+
+function gerarNumeroAleatorio () {
+    return Number.parseInt(Math.random() * 10 + 1);
 }
