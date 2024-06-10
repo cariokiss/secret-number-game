aa6207281b0863670c82f13a7ed1ea2797c0618e
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-10 10:14:34 -0300

    feat: verificando o chute

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10')
 
 function verificarChute() {
     const chute = document.querySelector('input').value;
-    console.log(chute === numeroSecreto)
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Parabéns, você descobriu o número secreto!')
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor')
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior')
+        }
+    }
 }
 
 function gerarNumeroAleatorio () {
